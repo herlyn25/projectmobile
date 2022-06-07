5844081d7f2f53fcac63e76c56dbcb5a067dc521
herlyn25 <herly-1988@hotmail.com>
2022-06-07 16:59:15 -0500

    Agregando el alert al proyecto

--- lib/view/listtile_custom.dart
+++ lib/view/listtile_custom.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clase1/models/games.dart';
 
 // ignore: must_be_immutable
 class ListTileCustom extends StatelessWidget {
   final String name;
   final String image;
-  ListTileCustom({super.key, required this.name, required this.image});
+  const ListTileCustom({super.key, required this.name, required this.image});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: CircleAvatar(backgroundImage: Image.network(image).image),
       title: Text(name),
-      onTap: () => Scaffold.of(context).showSnackBar(SnackBar(
-        content: Image(
-          image: Image.network(image).image,
-        ),
-        duration: const Duration(seconds: 2),
-      )),
+      onTap: () => showDialog<Image>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+              title: Text(name),
+              content: Image(image: Image.network(image).image))),
     );
   }
 }
