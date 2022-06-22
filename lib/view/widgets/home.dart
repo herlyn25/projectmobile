816915320c1e51c7ff148111d011b656f8839f59
herlyn25 <herly-1988@hotmail.com>
2022-06-22 16:30:35 -0500

    efecto burbujas

--- lib/view/widgets/home.dart
+++ lib/view/widgets/home.dart
@@ -1,14 +1,20 @@
+import 'package:animated_background/animated_background.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clase1/view/widgets/gradient_main.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
+    const bubblesOptions = BubbleOptions(bubbleCount: 70);
     return MaterialApp(
       home: Scaffold(
-          
           resizeToAvoidBottomInset: false,
           appBar: AppBar(
             backgroundColor: const Color.fromARGB(255, 82, 81, 81),
@@ -19,8 +25,11 @@ class Home extends StatelessWidget {
             children: [
               GradienteMain(double.infinity),
               Center(
-                child:
-                    SizedBox(child: Image.asset("assets/logo_autolavado.png")),
+                child: AnimatedBackground(
+                    behaviour: BubblesBehaviour(options: bubblesOptions),
+                    vsync: this,
+                    child: SizedBox(
+                        child: Image.asset("assets/logo_autolavado.png"))),
               ),
             ],
           )),
